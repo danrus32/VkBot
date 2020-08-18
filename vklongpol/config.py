fd87f729a18fe127298ab7c74557f080adaeb148
danrus32 <56735314+danrus32@users.noreply.github.com>
2020-08-18 16:02:26 +0300

    Update config.py

--- vklongpol/config.py
+++ vklongpol/config.py
@@ -3,8 +3,8 @@ import time
 import datetime
 import requests
 import re
-token = "837f569ff54c4bebaf57524d2a6f82ae6a0ab9bc1ab77a53ebc185a800ce1183f7c326b0bb462de01a438"
-grupid = "187553500"
+token = "token"
+grupid = "groupid"
 v = 5.95
 def RandomId():
     a = random.randint(1,100000)
