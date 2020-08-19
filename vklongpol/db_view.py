0893ce4cf7a3f8241731bd4bd32b30471361215f
danrus32 <56735314+danrus32@users.noreply.github.com>
2020-08-20 00:40:23 +0300

    Update db_view.py

--- vklongpol/db_view.py
+++ vklongpol/db_view.py
@@ -4,5 +4,3 @@ sql = db.cursor()
 
 for value in sql.execute("SELECT * FROM  Users"):
     print(value)
-sql.execute(f'UPDATE Users SET user_id == ? WHERE Balance  = ?',('498475069',50))
-db.commit()
